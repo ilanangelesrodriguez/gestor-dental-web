7d067655b64e62437740563d42b39d3bfbe86ef7
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2023-10-25 01:01:22 -0500

    feat: agregar seccion de asistencia

--- public/index.php
+++ public/index.php
@@ -81,12 +81,29 @@ include dirname($base_path) . '/templates/main.php';
     <div class="tratamientos__asistencia">
         <div class="tratamientos__main-title">
             <span class="tratamientos__main-span">
-                Tratamientos
+                Seguimiento
             </span>
             <h2 class="tratamientos__main-h2">
                 Sobre la asistencia sanitaria
             </h2>
+            <span class="tratamientos__main-span">
+                Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+            </span>
         </div>
+            <div class="tratamientos__asistencia-content">
+                <div class="tratamientos__asistencia-img">
+                    <?php
+                    $base_path = __DIR__;
+                    include dirname($base_path) . '/media/atention-clinic.svg';
+                    ?>
+                </div>
+                <div class="tratamientos__asistencia-cards">
+                    <div class="tratamientos__asistencia-card">a</div>
+                    <div class="tratamientos__asistencia-card">a</div>
+                    <div class="tratamientos__asistencia-card">a</div>
+                </div>
+
+            </div>
 
     </div>
 </section>
@@ -157,7 +174,24 @@ include dirname($base_path) . '/templates/main.php';
     
 </section>
 
+    <div id="navbar" class="navbar__footer">
+        <svg width="40" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path class="svg__path" fill="#6b7280" d="M13.45 2.533a2.25 2.25 0 0 0-2.9 0L3.8 8.228a2.25 2.25 0 0 0-.8 1.72v9.305c0 .966.784 1.75 1.75 1.75h3a1.75 1.75 0 0 0 1.75-1.75V15.25c0-.68.542-1.232 1.217-1.25h2.566a1.25 1.25 0 0 1 1.217 1.25v4.003c0 .966.784 1.75 1.75 1.75h3a1.75 1.75 0 0 0 1.75-1.75V9.947a2.25 2.25 0 0 0-.8-1.72l-6.75-5.694Z"/>
+        </svg>
 
+        <svg width="40" height="37" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path class="svg__path" fill="#6b7280" fill-rule="evenodd" d="M6 1a1.75 1.75 0 0 0-1.75 1.75V4H3a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.25V2.75A1.75 1.75 0 0 0 10 1H6Zm4.25 3V2.75A.25.25 0 0 0 10 2.5H6a.25.25 0 0 0-.25.25V4h4.5ZM3 5.5h10a.5.5 0 0 1 .5.5v1h-11V6a.5.5 0 0 1 .5-.5Zm-.5 3V13a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8.5H9V10H7V8.5H2.5Z" clip-rule="evenodd"/>
+        </svg>
+
+        <svg width="40" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path class="svg__path" fill="#6b7280" d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"/>
+            <path class="svg__path" fill="#6b7280" d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"/>
+        </svg>
+
+        <svg width="40" height="37" viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg">
+            <path class="svg__path" fill="#6b7280" d="M96 128a128 128 0 1 1 256 0a128 128 0 1 1-256 0zM0 482.3C0 383.8 79.8 304 178.3 304h91.4c98.5 0 178.3 79.8 178.3 178.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4c89.1 0 161.3 72.2 161.3 161.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9c19.7-26.6 31.3-59.5 31.3-95.1c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z"/>
+        </svg>
+    </div>
 
 <?php
 include dirname($base_path) . '/templates/footer.php';
