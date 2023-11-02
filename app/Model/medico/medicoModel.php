0db1cd5c4cc78b655ebc334581e6baa9f3f9e1f3
maxx1212 <maxx1212_2@hotmail.com>
2023-11-02 00:12:48 -0500

    set and add to especialidad. add attribute foto

--- app/Model/medico/medicoModel.php
+++ app/Model/medico/medicoModel.php
@@ -6,10 +6,12 @@ require("/app/Model/Login/UsuarioModel.php");
 class MedicoModel extends UsuarioModel{
 
     protected $especialidad;
+    protected $foto;
 
-    public function __construct($username,$nombre, $clave, $especialidad,$disponibilidad) {
+    public function __construct($username,$nombre, $clave, $especialidad,$disponibilidad,$foto) {
         parent::__construct($username,$nombre, $clave, "medico");
         $this->especialidad = $especialidad;
+        $this->foto = $foto;
     }
 
     public function getEspecialidad() {
@@ -18,11 +20,16 @@ class MedicoModel extends UsuarioModel{
     public function getDisponibilidad() {
         return $this->disponibilidad;
     }
-    public static function createMedico($username,$nombre, $clave, $especialidad,$disponibilidad) {
-        return new self($username,$nombre, $clave, $especialidad,$disponibilidad);
+    public function setEspecialidad($especialidad) {
+        $this->especialidad = $especialidad;
     }
-
-
+    public function addEspecialidad($especialidad) {
+        array_push($this->especialidad, $especialidad);
+    }
+    public function getFoto() {
+        return $this->foto;
+    }
+    public static function createMedico($username,$nombre, $clave, $especialidad,$disponibilidad,$foto) {
+        return new self($username,$nombre, $clave, $especialidad,$disponibilidad,$foto);
+    }
 }
-
-?>
