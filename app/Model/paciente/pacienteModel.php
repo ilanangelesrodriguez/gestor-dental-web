608ee2c9e8993bb7361b2bf3a974d31525537fc5
maxx1212 <maxx1212_2@hotmail.com>
2023-11-02 00:14:05 -0500

    add direccion

--- app/Model/paciente/pacienteModel.php
+++ app/Model/paciente/pacienteModel.php
@@ -3,11 +3,11 @@ namespace Model\paciente;
 require("/app/Model/Login/UsuarioModel.php");
 
 class PacienteModel extends UsuarioModel{
-    public function __construc($username,$nombre,$clave){
+    public function __construc($username,$nombre,$clave,$direccion){
         parent::__construct($username,$nombre,$clave,"paciente");
     }
-    public static function createPaciente($username,$nombre,$clave){
-        return new self($username,$nombre,$clave);
+    public static function createPaciente($username,$nombre,$clave,$direccion){
+        return new self($username,$nombre,$clave,$direccion);
     }
 }
 
