2ec1c96ef6c8b3fe189d62da59f7f6949ca9e453
JDB-16 <139655252+JDB-16@users.noreply.github.com>
2023-12-02 13:15:29 -0500

    CitaView.php

--- app/View/CitaView.php
+++ app/View/CitaView.php
@@ -12,12 +12,12 @@ class CitaView
                 <form class="cita__form" method="post" action="?action=procesarCita">
                     <input required class="input" name="fecha_cita" placeholder="Fecha de la cita" type="date">
                     <input required class="input" name="hora_cita" placeholder="Hora de la cita" type="time">
-                    <input required class="input" name="paciente_atend" placeholder="Paciente" type="text">
-                    <input required class="input" name="medico_encarg" placeholder="Médico" type="text">
-                    <input required class="input" name="diagnostico" placeholder="Diagnóstico" type="text">
-                    <input required class="input" name="estado" placeholder="Estado" type="text">
-                    <input required class="input" name="fecha_prox" placeholder="Fecha Próxima" type="date">
-                    <input required class="input" name="comentario" placeholder="Comentario" type="text">
+                    <input required class="input" name="paciente_atend" placeholder="Nombre del paciente" type="text">
+                    <input required class="input" name="medico_encarg" placeholder="Nombre del Médico" type="text">
+                    <input required class="input" name="diagnostico" placeholder="Diagnóstico del Médico" type="text">
+                    <input required class="input" name="estado" placeholder="Estado de la cita" type="text">
+                    <input required class="input" name="fecha_prox" placeholder="Fecha Próxima Cita" type="date">
+                    <input required class="input" name="comentario" placeholder="Comentario adicional del paciente" type="text">
                     <button type="submit" class="cita__button">Programar Cita</button>
                 </form>
             </div>
